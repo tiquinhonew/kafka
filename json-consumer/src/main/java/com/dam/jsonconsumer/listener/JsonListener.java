3149e1c853795db5a0ff37b343bf26220a36ad19
Douglas Moraes <douglas.am@gmail.com>
2024-05-01 17:20:54 -0300

    add json consumer

---
+++ json-consumer/src/main/java/com/dam/jsonconsumer/listener/JsonListener.java
@@ -0,0 +1,42 @@
+package com.dam.jsonconsumer.listener;
+
+import com.dam.jsonconsumer.model.Payment;
+import lombok.SneakyThrows;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+import static java.lang.Thread.sleep;
+
+@Log4j2
+@Component
+public class JsonListener {
+
+    @SneakyThrows
+    @KafkaListener(topics = "payment-topic", groupId = "create-group", containerFactory = "jsonContainerFactory")
+    public void antiFraud(@Payload Payment payment) {
+        log.info("JSON_LISTENER ::: Recebi o pagamento {}", payment.toString());
+        sleep(2000);
+
+        log.info("JSON_LISTENER ::: Validando fraude... ");
+        sleep(2000);
+
+        log.info("JSON_LISTENER ::: Compra aprovada... ");
+        sleep(3000);
+    }
+
+    @SneakyThrows
+    @KafkaListener(topics = "payment-topic", groupId = "pdf-group", containerFactory = "jsonContainerFactory")
+    public void pdfGenerator(@Payload Payment payment) {
+        log.info("JSON_LISTENER ::: Gerando PDF de id {}... ", payment.getId());
+        sleep(3000);
+    }
+
+    @SneakyThrows
+    @KafkaListener(topics = "payment-topic", groupId = "email-group", containerFactory = "jsonContainerFactory")
+    public void sendEmail() {
+        sleep(3000);
+        log.info("JSON_LISTENER ::: Enviando email de confirmação... ");
+    }
+}
---
+++ json-consumer/src/test/java/com/dam/jsonconsumer/JsonConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dam.jsonconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
